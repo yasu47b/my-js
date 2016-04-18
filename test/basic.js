a36f1b5beec5aaaf13903a1fa7db236d47c07dc7
yasu47b <yasu47b@gmail.com>
2016-04-18 23:38:29 +0900

    Fix: add test if statement

--- test/basic.js
+++ test/basic.js
@@ -4,6 +4,7 @@ var sayHello = require('../src/module.js');
 describe('hello test', function(){
     it('sayHello function', function(){
         assert.equal('hello', sayHello());
+        assert.equal('hello', sayHello('hello'));
 	assert.notEqual('bye', sayHello());
     });
 });
